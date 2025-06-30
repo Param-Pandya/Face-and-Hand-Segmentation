0b97592212979bbe53f18629157be7fb3abe3a08
Param Pandya <97044527+Param-Pandya@users.noreply.github.com>
2025-06-30 23:20:09 +0530

    Update sam2_infer.py

--- sam2_infer.py
+++ sam2_infer.py
@@ -1,16 +1,27 @@
 import replicate
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 def call_sam2(image_path, boxes):
-    replicate_client = replicate.Client(api_token=os.getenv("REPLICATE_API_TOKEN"))
-    model = replicate.models.get("meta/sam2")
-    version = model.versions.get("latest")
+    replicate_client = replicate.Client(
+    api_token=os.getenv("REPLICATE_API_TOKEN"),
+    timeout=60  
+    )
 
-    prompt_boxes = [{"type": "box", "box": b} for b in boxes]
+    model = replicate_client.models.get("lucataco/segment-anything-2")
+    version = model.versions.list()[0]
+
+    prompt_boxes = [{"type": "box", "box": [int(x) for x in b]} for b in boxes]
 
     with open(image_path, "rb") as f:
-        output = version.predict(image=f, input_boxes=prompt_boxes)
+        output = replicate_client.run(
+            version,
+            input={
+                "image": f,
+                "input_boxes": prompt_boxes
+            }
+        )
 
     return output["masks"]
